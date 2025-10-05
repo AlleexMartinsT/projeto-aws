a8228da16350ce50df8ef545e5ad88195fb0b7de
AlleexMartinsT <106168113+AlleexMartinsT@users.noreply.github.com>
2025-10-05 19:41:24 -0300

    Reiniciando o repositório do zero

--- salvar_alunos/handler.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    for record in event['Records']:
-        aluno = json.loads(record['Sns']['Message'])
-        print(f"Salvando aluno: {aluno['nome']} - Matrícula: {aluno['matricula']}")
-
-    return {'statusCode': 200, 'body': json.dumps('Alunos salvos com sucesso')}
